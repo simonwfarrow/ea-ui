d6e6a95e09b085ca263e092f6aef634e570c19ea
Simon Farrow <Simon.Farrow@fisglobal.com>
2023-03-04 17:49:36 +0000

    initial commit

--- src/routes/ea/services/+page.svelte
+++ src/routes/ea/services/+page.svelte
@@ -3,13 +3,13 @@
 
     import { ServiceDescriptor, ServicesRepositoryGitHub } from "@electronic-architect/ea-services";
 
-    let repo = new ServicesRepositoryGitHub();
-    repo.getServices({
+    let repo = new ServicesRepositoryGitHub({
         url: 'https://api.github.com',
         token: 'Bearer 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',
         owner: 'simonwfarrow',
         repo: 'ea-resources'
-    }).then(services => {
+    });
+    repo.getServices().then(services => {
        services.forEach(service => {
            console.log(service.name);
        })
