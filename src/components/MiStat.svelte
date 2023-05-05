6b7d1b4fd14132f20a81e17aaaa411a600c226d6
Olivier Chalet <olivier.chalet@fisglobal.com>
2023-05-05 15:53:26 +0100

    US000000: change Query screen to display MI ADRs information

---
+++ src/components/MiStat.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let title;
+  export let value;
+</script>
+
+<div class="card variant-glass-primary p-4 shadow !text-white mb-10">
+  <dl class="list-dl">
+    <div>
+      <span class="badge bg-primary-500">&nbsp;i&nbsp;</span>
+      <span class="flex-auto">
+          <dt><b>{title}</b></dt>
+          <dd>{value}</dd>
+		    </span>
+    </div>
+  </dl>
+</div>
--- src/routes/ea/query/+page.svelte
+++ src/routes/ea/query/+page.svelte
@@ -1,24 +1,182 @@
 <script lang="ts">
-    import {serviceStore} from "../../../stores/service";
-    import {flowStore} from "../../../stores/flow";
+  import {
+    ArcElement,
+    CategoryScale,
+    Chart as ChartJS,
+    Colors,
+    Legend,
+    Title,
+    Tooltip,
+  } from 'chart.js';
+  import Chart from 'chart.js/auto';
+  import { onMount } from 'svelte';
+  import { derived, writable } from 'svelte/store';
+  import MiStat from '../../../components/MiStat.svelte';
 
-    let serviceArray = [];
+  ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale, Colors);
 
+  const BEARER_TOKEN = '';
+  const apiBaseUrl = 'https://github.worldpay.com/api/v3/repos/access-for-ecom-doc/adrs';
+  const apiHeaders = {
+    headers: {
+      'Authorization': `Bearer ${BEARER_TOKEN}`,
+    },
+  };
+  let ctx;
+  let chartCanvas;
+
+  const allADRs = writable([]);
+
+  class ADR {
+    title: string;
+    state: string;
+    createdAtUTC: Date;
+    closedAtUTC: Date;
+    isOpen: boolean;
+    isClosed: boolean;
+    ttrInDays: number = 0;
+    milestone: string;
+    isIdentified:boolean;
+    isProposed:boolean;
+    isAccepted: boolean;
+    isBeingProved: boolean;
+    isAdopted: boolean;
+    isRejected: boolean;
+
+    constructor(dto) {
+      this.title = dto.title;
+      this.state = dto.state;
+      this.createdAtUTC = new Date(dto.created_at);
+      this.closedAtUTC = new Date(dto.closed_at);
+      this.isOpen = this.state === 'open';
+      this.isClosed = this.state === 'closed';
+      this.ttrInDays = this.calculateTimeToResolutionInDays();
+      this.milestone = dto.milestone ? dto.milestone.title : '';
+      this.isIdentified = this.milestone === 'Identified';
+      this.isProposed = this.milestone === 'Proposed';
+      this.isAccepted = this.milestone === 'Accepted';
+      this.isBeingProved = this.milestone === 'Prove';
+      this.isAdopted = this.milestone === 'Adopted';
+      this.isRejected = this.milestone === 'Rejected';
+    }
+
+    calculateTimeToResolutionInDays(): number {
+      if (this.isOpen) {
+        return 0;
+      }
+      return (this.closedAtUTC.getTime() - this.createdAtUTC.getTime()) / (24 * 3600 * 1000);
+    }
+  }
+
+  const openADRs = derived(allADRs, ($allADRs) => {
+    return $allADRs.filter(adr => adr.isOpen).length;
+  });
+
+  const closedADRs = derived(allADRs, ($allADRs) => {
+    return $allADRs.filter(adr => adr.isClosed).length;
+  });
+
+  const identifiedADRs = derived(allADRs, ($allADRs) => {
+    return $allADRs.filter(adr => adr.isIdentified).length;
+  });
+
+  const proposedADRs = derived(allADRs, ($allADRs) => {
+    return $allADRs.filter(adr => adr.isProposed).length;
+  });
+
+  const acceptedADRs = derived(allADRs, ($allADRs) => {
+    return $allADRs.filter(adr => adr.isAccepted).length;
+  });
+
+  const provingADRs = derived(allADRs, ($allADRs) => {
+    return $allADRs.filter(adr => adr.isBeingProved).length;
+  });
+
+  const adoptedADRs = derived(allADRs, ($allADRs) => {
+    return $allADRs.filter(adr => adr.isAdopted).length;
+  });
+
+  const rejectedADRs = derived(allADRs, ($allADRs) => {
+    return $allADRs.filter(adr => adr.isRejected).length;
+  });
+
+  const meanTimeToResolution = derived(allADRs, ($allADRs) => {
+    const closedADRs = $allADRs.filter(adr => adr.isClosed);
+    if (closedADRs.length === 0) {
+      return 0;
+    }
+    return Math.round(closedADRs.reduce((total, adr) => total + adr.ttrInDays, 0) / closedADRs.length);
+  });
+
+  function mountPieChart() {
+    const chartOptions = {
+      responsive: true,
+      plugins: {
+        legend: {
+          labels: {
+            color: 'white',
+          },
+        },
+      },
+    };
+
+    new Chart(chartCanvas.getContext('2d'), {
+      type: 'pie',
+      data: {
+        labels: ['Identified', 'Proposed', 'Accepted', 'Being proved', 'Adopted', 'Rejected'],
+        datasets: [
+          {
+            label: ' Number ADRs ',
+            data: [$identifiedADRs, $proposedADRs, $acceptedADRs, $provingADRs, $adoptedADRs, $rejectedADRs],
+          },
+        ],
+      },
+      options: chartOptions,
+    });
+  }
+
+  onMount(async () => {
+    const searchFor = 'all';
+    const pageSize = 100;
+
+    const response = await fetch(`${apiBaseUrl}/issues?state=${searchFor}&per_page=${pageSize}`, apiHeaders);
+    const issuesExcludingPRs = (await response.json())
+      .filter(issue => issue.pull_request === undefined);
+
+    allADRs.set(issuesExcludingPRs.map(dto => new ADR(dto)));
+
+    mountPieChart();
+  });
 </script>
 
-{#each serviceArray as result}
-<div class="card variant-glass-primary p-4 shadow !text-white">
-    <dl class="list-dl">
-        <div>
-            <span class="badge bg-primary-500">💀</span>
-            <span class="flex-auto">
-			<dt>Title</dt>
-			<dd>Description</dd>
-		</span>
-        </div>
-        <!-- ... -->
-    </dl>
+
+<style>
+  .col {
+    width: 50%;
+    float: left;
+  }
+</style>
+
+<div>
+  <div class="col">
+    <MiStat title="Number of ADRs currently open" value={$openADRs} />
+    <MiStat title="Number of ADRs identified" value={$identifiedADRs} />
+    <MiStat title="Number of ADRs proposed" value={$proposedADRs} />
+    <MiStat title="Number of ADRs accepted" value={$acceptedADRs} />
+    <MiStat title="Number of ADRs being proved" value={$provingADRs} />
+    <MiStat title="Number of ADRs adopted" value={$adoptedADRs} />
+    <MiStat title="Number of ADRs rejected" value={$rejectedADRs} />
+    <MiStat title={`Mean Time To Resolution`}
+            value={`${$meanTimeToResolution} days (${$closedADRs} ADRs closed)`} />
+  </div>
+  <div class="col pl-20 pr-20">
+    <canvas bind:this={chartCanvas} id="myChart" />
+  </div>
 </div>
-{/each}
+
+
+<!--{#each $allADRs as adr}-->
+<!--  <MiStat title={`${adr.title} (${adr.milestone})`} value={0} />-->
+<!--{/each}-->
 
 
